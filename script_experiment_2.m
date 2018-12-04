1e665c3fd7700de93aa856a0f6e3062fd313b9b8
jasminefish000 <haojasmine@gmail.com>
2018-12-03 22:21:31 -0800

    Create experiments

--- script_experiment_2.m
+++ script_experiment_2.m
@@ -1,3 +1,4 @@
+% Use this experiments as an example
 if ~exist("conAssign")
     run C:\tomlab\startup.m
 else
@@ -5,10 +6,12 @@ else
 end
 addpath(genpath(pwd));
 run gen_param.m
-max_iter=100;
+max_iter=1;
 %% Initialize estimators
+% This experiment mainly shows that as the 
 estimator_list = {'FD','FD2','AFD','AFD2','HM','EE'};
-statistic_list = {'average','bias','var'};
+statistic_list = {'average','bias','var','time','iter'};
+N_list = [100,1000];
 norm_p=[];
 norm_p_modified=[];
 
@@ -18,9 +21,10 @@ for estimator = estimator_list
     end
 end
 
-for gamma_a = 0 : 0.5 : 2.0
-% for gamma_a = 2.0 : 0.5 : 3.0
-     param.gamma.gamma_a = gamma_a;
+for n = N_list
+    
+    param.gamma.gamma_a = 5;
+    param.nM=n;param.N=n;
     [F_struct,state] = DDCMixture.statetransition(param); %Generate
     
     F_0 = kron([0,1;0,1],F_struct{2});
@@ -34,15 +38,10 @@ for gamma_a = 0 : 0.5 : 2.0
     Result = tomRun('snopt', Prob);
     p_star = Result.x_k;
     
-    
-%     S1.N     = param.nM;
-%     S1.T     = param.nT;
-%     S1.beta  = param.beta;
     param.P     = F_struct;
     param.state = state;
     param.n_type = 1;
     param.n_action = 2;
-%     param.n_state = size(S1.P{1},2);
     
     theta_vec = [theta.VP0,theta.VP1,theta.VP2,theta.FC0,theta.FC1,theta.EC0,theta.EC1]';
     ts = tic;
@@ -60,8 +59,7 @@ for gamma_a = 0 : 0.5 : 2.0
     theta_vec0 = zeros(7,1);
     p_default = zeros(64,1);
 
-    % for i = 1:param.nMC
-    % Method = {'EE','HM','FD','AFD2'}
+ 
     parfor i = 1:param.nMC
         opt = struct();
         fprintf('Estimating sample %d out of %d\n', i, param.nMC);
@@ -125,23 +123,28 @@ for gamma_a = 0 : 0.5 : 2.0
         eval(['average_' estimator{1} '=[average_' estimator{1}  ' transpose(mean(abs(ResultTable_' estimator{1} ' - transpose(theta_vec))))]; ']);
         eval(['bias_' estimator{1} '=[bias_' estimator{1}  ' transpose(abs(mean(ResultTable_' estimator{1} ' - transpose(theta_vec))))]; ']);
         eval(['var_' estimator{1} '=[var_' estimator{1}  ' transpose(var(ResultTable_' estimator{1} ' - transpose(theta_vec)))]; ']);
+        eval(['time_' estimator{1} '=[time_' estimator{1}  ' transpose(TimeTable_' estimator{1} ')]; ']);
+        eval(['iter_' estimator{1} '=[iter_' estimator{1}  ' transpose(IterTable_' estimator{1} ')]; ']);
     end
 end
 %% Diary Session
-diarystr = sprintf('diary/Table_sequential_gammaa_%d_M%d_T%d.txt',param.nGrid,param.nM,param.nT);
+diarystr = sprintf('diary/Table_2step_N_%d_M%d_T%d.txt',param.nGrid,param.nM,param.nT);
 delete(diarystr);
 diary(diarystr);
+disp(['This experiment uses 2 step estimator with different values of'...
+    '$\gamma_a$. The size of the sample is N=100, T=120 with 500 Monte Carlo'...
+    'Simulations.']);
 
-input.data = [0 : 0.5 : 2.0; norm_p;norm_p_modified];
-input.tableRowLabels = {'$\gamma_a$', 'norm before modified', 'norm after modified'}; 
-input.tableColLabels = { '','','','','' };
+input.data = [norm_p;norm_p_modified];
+input.tableRowLabels = {'norm before modified', 'norm after modified'}; 
+input.tableColLabels = num2cell(N_list);
 input.tableCaption = 'The norm of the differences in transition densities';
 latexTable(input);
 
 
 % Bias and Variance Table
 input.tableRowLabels = param_cell;
-input.tableColLabels = {'0','0.5','1.0','1.5','2.0'};
+input.tableColLabels = num2cell(N_list);
 
 
 for estimator = estimator_list
@@ -155,16 +158,15 @@ end
 
 command_str_time = '[';command_str_iter = '[';
 for estimator = estimator_list
-    command_str_time = [command_str_time 'mean(TimeTable_',estimator{1},') '];
-    command_str_iter = [command_str_iter 'mean(IterTable_',estimator{1},') '];
+    command_str_time = [command_str_time '; mean(time_',estimator{1},') '];
+    command_str_iter = [command_str_iter '; mean(iter_',estimator{1},') '];
 end
 command_str_time = [command_str_time ']'];command_str_iter = [command_str_iter ']'];
 
-
-average_time = [eval(command_str_time);eval(command_str_iter)];
-input.data = average_time;
-input.tableColLabels = estimator_list;
-input.tableRowLabels = {'Average Time','Average Iter'};
+input.data = eval(command_str_time);
+input.variance = eval(command_str_iter);
+input.tableColLabels = num2cell(N_list);
+input.tableRowLabels = estimator_list;
 input.tableCaption = 'The averaged time used';
-latexTable(input);
+latexVarianceTable(input);
 diary off;
