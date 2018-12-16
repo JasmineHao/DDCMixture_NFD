61d1d95e88cc1dd1788a97136112933704fb2a72
Jasmine Hao <haojasmine@gmail.com>
2018-12-16 11:07:55 -0800

    update results

--- script_experiment_1_seq.m
+++ script_experiment_1_seq.m
@@ -67,6 +67,7 @@ for gamma_a = gamma_a_list
 
 %%
     parfor i = 1:param.nMC
+        opt.true_ccp=0;
         opt = struct();
         fprintf('Estimating sample %d out of %d\n', i, param.nMC);
         datasim = Data{i};
